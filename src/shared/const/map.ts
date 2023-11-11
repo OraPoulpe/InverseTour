b1e5fe5e7a0f089c966a84d8f1ac77cf17013ffd
”OraPoulpe” <ssazonov69@gmail.com>
2023-11-11 06:29:21 +0300

    fix dug with map

--- src/shared/const/map.ts
+++ src/shared/const/map.ts
@@ -1,2 +1,2 @@
-export const API_KEY = '5390b679-c914-47e0-88a8-5ca19b3c2764';
-// export const API_KEY = 'e85a80dc-691e-4fe7-b580-455966e5e46c';
+// export const API_KEY = '5390b679-c914-47e0-88a8-5ca19b3c2764';
+export const API_KEY = 'e85a80dc-691e-4fe7-b580-455966e5e46c';
